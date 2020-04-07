5eaa629eb311deba18f071903460bcf5eb4e9514
omerkeskin <omer.keskin.deu@hotmail.com>
2020-04-07 23:16:14 +0300

    Computed

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -3,7 +3,8 @@ var app = new Vue({
     data: {
         message: 'Hello Vue!',
         x :1,
-        y :2
+        y :2,
+        counter :0
     },
     methods: {
         showAlert: function (event, name) {
@@ -12,13 +13,34 @@ var app = new Vue({
         updateCoords: function (event) {
            this.x = event.clientX;
            this.y = event.clientY;
+        },
+        onTxtMyCustomKeyUp: function (event) {
+            console.log('Target : '+event.target.value);
+        },
+        calculateResult:function () {
+            console.log('Here we go calculateResult');
+            return  this.counter > 10 ? '10 dan büyük' : '10 dan küçük';
+        }
+    },
+    computed: {
+        firstCounterResult : function () {
+            console.log('Here we go firstCounterResult');
+            return  this.counter > 10 ? '10 dan büyük' : '10 dan küçük';
         }
     }
+
+
+
 });
 
 /*document.querySelector('#p_mycustom').addEventListener('mousemove', function (event) {
    console.log('X : '+ event.clientX + ' | Y : '+ event.clientY)
 });*/
+
+/*function onTxtMyCustomChange(event){
+    var txtValue = document.getElementById("txt_mycustom").value;
+    console.log(txtValue);
+}*/
 
 document.querySelector('#btn_mycustom').addEventListener('click', function () {
     console.log('btn_mycustom clicked');
